cc43f367dbc07b0b909b9463f18be92338a7f074
asok <adam.sokolnicki@gmail.com>
2012-10-15 15:10:51 +0200

    Dry up my-rails-mode-helm-projectile.

--- my-rails-mode/my-rails-mode-helm-projectile.el
+++ my-rails-mode/my-rails-mode-helm-projectile.el
@@ -1,82 +1,58 @@
 (require 'helm-projectile)
 
-(defmacro mrm/helm-projectile-c-source (function-name)
-    `(list '(name . "Files")
-         ;; Needed for filenames with capitals letters.
-         '(disable-shortcuts)
-         '(candidates . ,function-name)
-         '(volatile)
-         '(keymap . ,helm-generic-files-map)
-         '(help-message . helm-generic-file-help-message)
-         '(match  helm-c-match-on-basename)
-         '(mode-line . helm-generic-file-mode-line-string)
-         '(type . file)))
+(defmacro mrm/def-helm-c-source (name)
+  `(defvar ,(intern (format "mrm/helm-c-source-%S-files-list" name))
+     (list '(name . ,(format "%S" name))
+	   ;; Needed for filenames with capitals letters.
+	   '(disable-shortcuts)
+	   '(candidates . ,(intern (format "mrm/%S-files-list" name)))
+	   '(volatile)
+	   '(keymap . ,helm-generic-files-map)
+	   '(help-message . helm-generic-file-help-message)
+	   '(match  helm-c-match-on-basename)
+	   '(mode-line . helm-generic-file-mode-line-string)
+	   '(type . file))))
 
-(defun mrm/helm-c-projectile-files-list (path)
-  (projectile-get-project-files
-   (concat (mrm/root) path)))
+(defmacro mrm/def-files-list (name path)
+  `(defun ,(intern (format "mrm/%S-files-list" name)) ()
+     (projectile-get-project-files (concat (mrm/root) ,path))))
 
-;; (defun mrm/helm-c-projectile-models-current-scope-files-list ()
-;;   (cond
-;;    ((mrm/under-p "controllers")
-;;     ()
-;;     )
+(mrm/def-files-list models "app/models/")
+(mrm/def-files-list views "app/views/")
+(mrm/def-files-list controllers "app/controllers/")
+(mrm/def-files-list specs "spec/")
 
-;;    )
-;;   )
-
-(defun mrm/helm-c-projectile-models-files-list ()
-  (mrm/helm-c-projectile-files-list "app/models/"))
-
-(defun mrm/helm-c-projectile-controllers-files-list ()
-  (mrm/helm-c-projectile-files-list "app/controllers/"))
-
-(defun mrm/helm-c-projectile-views-files-list ()
-  (mrm/helm-c-projectile-files-list "app/views/"))
-
-(defun mrm/helm-c-projectile-specs-files-list ()
-  (mrm/helm-c-projectile-files-list "spec/"))
-
-(defvar mrm/helm-projectile-c-source-models-file-list
- (mrm/helm-projectile-c-source mrm/helm-c-projectile-models-files-list))
-;; (defvar mrm/helm-projectile-c-source-models-current-scope-file-list
-;;  (mrm/helm-projectile-c-source mrm/helm-c-projectile-models-current-scope-files-list))
-
-(defvar mrm/helm-projectile-c-source-controllers-file-list
- (mrm/helm-projectile-c-source mrm/helm-c-projectile-controllers-files-list))
-
-(defvar mrm/helm-projectile-c-source-views-file-list
- (mrm/helm-projectile-c-source mrm/helm-c-projectile-views-files-list))
-
-(defvar mrm/helm-projectile-c-source-specs-file-list
- (mrm/helm-projectile-c-source mrm/helm-c-projectile-specs-files-list)) 
+(mrm/def-helm-c-source models)
+(mrm/def-helm-c-source views)
+(mrm/def-helm-c-source controllers)
+(mrm/def-helm-c-source specs)
 
 ;;;###autoload
 (defun mrm/helm-projectile-models ()
   "Search using helm for models"
   (interactive)
-  (helm-other-buffer '(mrm/helm-projectile-c-source-models-file-list)
+  (helm-other-buffer '(mrm/helm-c-source-models-files-list)
                      (format "*My Rails Mode %s*" "models" )))
 
 ;;;###autoload
 (defun mrm/helm-projectile-views ()
   "Search using helm for views"
   (interactive)
-  (helm-other-buffer '(mrm/helm-projectile-c-source-views-file-list)
+  (helm-other-buffer '(mrm/helm-c-source-views-files-list)
                      (format "*My Rails Mode %s*" "views" )))
 
 ;;;###autoload
 (defun mrm/helm-projectile-controllers ()
   "Search using helm for controllers"
   (interactive)
-  (helm-other-buffer '(mrm/helm-projectile-c-source-controllers-file-list)
+  (helm-other-buffer '(mrm/helm-c-source-controllers-files-list)
                      (format "*My Rails Mode %s*" "controllers" )))
 
 ;;;###autoload
 (defun mrm/helm-projectile-specs ()
   "Search using helm for specs"
   (interactive)
-  (helm-other-buffer '(mrm/helm-projectile-c-source-specs-file-list)
+  (helm-other-buffer '(mrm/helm-c-source-specs-files-list)
                      (format "*My Rails Mode %s*" "specs" )))
 
 (provide 'my-rails-mode-helm-projectile)
