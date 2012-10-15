7598c7a601bee398fbc25fa3af59788ce08ee352
asok <adam.sokolnicki@gmail.com>
2012-10-15 16:55:35 +0200

    Add mrm/helm-projectile-all.

--- my-rails-mode/my-rails-mode-helm-projectile.el
+++ my-rails-mode/my-rails-mode-helm-projectile.el
@@ -15,17 +15,24 @@
 
 (defmacro mrm/def-files-list (name path)
   `(defun ,(intern (format "mrm/%S-files-list" name)) ()
-     (projectile-get-project-files (concat (mrm/root) ,path))))
+     (projectile-get-project-files (concat (projectile-get-project-root) ,path))))
+
 
 (mrm/def-files-list models "app/models/")
 (mrm/def-files-list views "app/views/")
 (mrm/def-files-list controllers "app/controllers/")
+(mrm/def-files-list helpers "app/helpers/")
 (mrm/def-files-list specs "spec/")
+(mrm/def-files-list javascripts "public/javascripts/")
+(mrm/def-files-list stylesheets "public/stylesheets/")
 
 (mrm/def-helm-c-source models)
 (mrm/def-helm-c-source views)
 (mrm/def-helm-c-source controllers)
+(mrm/def-helm-c-source helpers)
 (mrm/def-helm-c-source specs)
+(mrm/def-helm-c-source javascripts)
+(mrm/def-helm-c-source stylesheets)
 
 ;;;###autoload
 (defun mrm/helm-projectile-models ()
@@ -54,5 +61,21 @@
   (interactive)
   (helm-other-buffer '(mrm/helm-c-source-specs-files-list)
                      (format "*My Rails Mode %s*" "specs" )))
+
+;;;###autoload
+(defun mrm/helm-projectile-all (&optional arg)
+  "Search using helm for models, views, controllers, helpers, specs, js and stylesheets files.
+   If called with optional arg uses `helm-projectile' for show all files in the project."
+  (interactive "P")
+  (if (consp arg)
+      (helm-projectile)
+  (helm-other-buffer '(mrm/helm-c-source-models-files-list
+		       mrm/helm-c-source-views-files-list
+		       mrm/helm-c-source-controllers-files-list
+		       mrm/helm-c-source-helpers-files-list
+		       mrm/helm-c-source-specs-files-list
+		       mrm/helm-c-source-javascripts-files-list
+		       mrm/helm-c-source-stylesheets-files-list)
+                     (format "*My Rails Mode %s*" "specs" ))))
 
 (provide 'my-rails-mode-helm-projectile)
