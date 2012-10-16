adeaf5c53a7b39a46e4919200c0ef563c283419e
asok <adam.sokolnicki@gmail.com>
2012-10-16 16:43:36 +0200

    Add el-get bootstrap.

--- inits/packages.el
+++ inits/packages.el
@@ -1,3 +1,10 @@
+(unless (file-exists-p "~/.emacs.d/el-get")
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (goto-char (point-max))
+     (eval-print-last-sexp))))
+
 (require 'package)
 (package-initialize)
 
