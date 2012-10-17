93bc02a6f6dad5863e567ffd6f86b0f60416e954
asok <adam.sokolnicki@gmail.com>
2012-10-17 12:39:05 +0200

    Use helm-projectile for mrm/helm-projectile-all. Display only relative paths in the mrm/helm-c-sources.

--- my-rails-mode/my-rails-mode-helm-projectile.el
+++ my-rails-mode/my-rails-mode-helm-projectile.el
@@ -1,6 +1,7 @@
 (require 'helm-projectile)
 
-(defmacro mrm/def-helm-c-source (name)
+
+(defmacro mrm/def-helm-c-source (name path)
   `(defvar ,(intern (format "mrm/helm-c-source-%S-files-list" name))
      '((name . ,(format "%S" name))
        ;; Needed for filenames with capitals letters.
@@ -10,12 +11,20 @@
        (keymap . ,helm-generic-files-map)
        (help-message . helm-generic-file-help-message)
        (match  helm-c-match-on-basename)
+       (candidate-number-limit . 25)
        (mode-line . helm-generic-file-mode-line-string)
+       (action . (lambda (candidate)
+		   (find-file (concat (projectile-get-project-root) ,path candidate))))
        (type . file))))
 
 (defmacro mrm/def-files-list (name path)
   `(defun ,(intern (format "mrm/%S-files-list" name)) ()
-     (projectile-get-project-files (concat (projectile-get-project-root) ,path))))
+     (let ((subdirpath (concat (expand-file-name (projectile-get-project-root)) ,path)))
+       (mapcar (lambda (c)
+		 (substring c (length subdirpath)))
+	       (projectile-get-project-files subdirpath))
+       )
+     ))
 
 (defmacro mrm/def-helm-projectile (name)
 ;;;###autoload
@@ -35,37 +44,41 @@
 	(javascripts  "public/javascripts/")
 	(stylesheets  "public/stylesheets/"))
       do(progn
-	  (eval `(mrm/def-helm-c-source ,(first pair)))
+	  (eval `(mrm/def-helm-c-source ,(first pair) ,(second pair)))
 	  (eval `(mrm/def-files-list ,(first pair) ,(second pair)))
 	  (eval `(mrm/def-helm-projectile ,(first pair)))))
 
-(mrm/def-helm-c-source various)
-(defun mrm/various-files-list ()
-  (let ((projectile-ignored-directories '("tmp"
-					   "public"
-					   "coverage"
-					   "db/migrate"
-					   "app"
-					   "spec")))
-    (projectile-get-project-files (projectile-get-project-root))
-    ))
+;(mrm/def-helm-c-source various )
+;(defun mrm/various-files-list ()
+;  (let ((projectile-ignored-directories '("tmp"
+;					   "public"
+;					   "coverage"
+;					   "vendor"
+;					   "app"
+;					   "spec")))
+;    (mapcar (lambda (c)
+;	      (car (last (split-string c "/"))))
+;	    (projectile-get-project-files (projectile-get-project-root)))
+;    ))
 
 ;;;###autoload
-(defun mrm/helm-projectile-all (&optional arg)
-  "Search using helm for models, views, controllers, helpers, specs, js and stylesheets files.
-   If called with optional arg uses `helm-projectile' for show all files in the project."
-  (interactive "P")
-  (if (consp arg)
-      (helm-projectile)
-    (helm-other-buffer '(mrm/helm-c-source-models-files-list
-			 mrm/helm-c-source-views-files-list
-			 mrm/helm-c-source-controllers-files-list
-			 mrm/helm-c-source-helpers-files-list
-			 mrm/helm-c-source-mailers-files-list
-			 mrm/helm-c-source-specs-files-list
-			 mrm/helm-c-source-javascripts-files-list
-			 mrm/helm-c-source-stylesheets-files-list
-			 mrm/helm-c-source-various-files-list)
-		       (format "*My Rails Mode %s*" "specs" ))))
+(defun mrm/helm-projectile-all () ;(&optional arg)
+  (helm-projectile)
+  ;"Search using helm for models, views, controllers, helpers, specs, js and stylesheets files.
+   ;If called with optional arg uses `helm-projectile' for show all files in the project."
+  ;(interactive "P")
+  ;(if (consp arg)
+    ;(helm-other-buffer '(mrm/helm-c-source-models-files-list
+			 ;mrm/helm-c-source-views-files-list
+			 ;mrm/helm-c-source-controllers-files-list
+			 ;mrm/helm-c-source-helpers-files-list
+			 ;mrm/helm-c-source-mailers-files-list
+			 ;mrm/helm-c-source-specs-files-list
+			 ;mrm/helm-c-source-javascripts-files-list
+			 ;mrm/helm-c-source-stylesheets-files-list
+			 ;mrm/helm-c-source-various-files-list)
+		       ;(format "*My Rails Mode %s*" "specs" ))
+    ;(helm-projectile))
+  )
 
 (provide 'my-rails-mode-helm-projectile)
