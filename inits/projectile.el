0b5d7027fe9243bde57b2af3644019760d727301
asok <adam.sokolnicki@gmail.com>
2012-10-17 14:39:50 +0200

    Comment the projectile hook after all.

--- inits/projectile.el
+++ inits/projectile.el
@@ -3,6 +3,6 @@
 (add-hook 'rhtml-mode-hook #'(lambda () (projectile-mode)))
 (setq projectile-ignored-directories (append projectile-ignored-directories '("tmp" "public" "coverage" "log" "vendor" "db/migrate")))
 
-(add-hook 'magit-checkout-command-hook '(lambda () (projectile-invalidate-cache)))
+;; (add-hook 'magit-checkout-command-hook '(lambda () (projectile-invalidate-cache)))
 
 (provide 'inits/projectile)
